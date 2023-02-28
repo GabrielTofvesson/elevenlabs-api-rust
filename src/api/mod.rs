d3b88bb4e137ecaf97b9d880435fed762a738ef0
Gabriel Tofvesson <contact@w1zzrd.dev>
2023-02-28 01:02:55 +0100

    Implement tts generation

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,2 +1,3 @@
 pub mod user;
 pub mod history;
+pub mod tts;
---
+++ src/api/tts.rs
@@ -0,0 +1,18 @@
+use crate::{elevenlabs_api::ElevenLabsAPI, model::{history::AudioItem, error::{HTTPValidationError, APIError}}};
+
+
+
+impl ElevenLabsAPI {
+    pub async fn generate_tts(&self, voice_id: String, text: String) -> Result<AudioItem, APIError> {
+        let response = self.post(crate::elevenlabs_api::tts::POST::File { voice_id })?.json(&text).send().await?;
+
+        if response.status().is_success() {
+            Ok(AudioItem::new_single(response.bytes().await?))
+        } else {
+            let error: HTTPValidationError = response.json().await?;
+            Err(APIError::HTTPError(error))
+        }
+    }
+
+    // TODO: Consider stream implementation
+}
